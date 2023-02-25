b362e17c099f0cd0a42ad29654fcd30e07c9f4dc
web scrapping (reviews) <gawandeadi2@gmail.com>
2023-02-26 03:50:16 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
+import pymongo
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
@@ -72,6 +73,14 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            
+            client = pymongo.MongoClient("mongodb+srv://gawande:<password>@cluster0.nwpd5hw.mongodb.net/?retryWrites=true&w=majority")
+            
+
+            db=client['review_scraper']
+            review_col=db['review_scrap_data']
+            review_col.insert_many(reviews)
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
